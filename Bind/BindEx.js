33a9bf7d2a8dc542c46423a783a7bb23994efe16
khoi <khoimap2307@gmail.com>
2026-05-05 10:10:29 +0700

    bind

--- Bind/BindEx.js
+++ Bind/BindEx.js
@@ -9,3 +9,14 @@ const teacher = {
 const button = document.querySelector('button')
 
 button.onclick = teacher.getFullName.bind(teacher)
+
+const doctor = {
+    nickName : "Nick",
+    fullName : "Leonardo Da Venci",
+    getInfo() {
+        console.log(`${this.nickName} ${this.fullName}`)
+    }
+}
+
+const buttonClick = document.querySelector('#click')
+buttonClick.onclick = doctor.getInfo.bind(doctor)
---
+++ Bind/Bindexp.js
@@ -0,0 +1,6 @@
+// const $ = document.querySelector.bind(document)
+// const $$ = document.querySelectorAll.bind(document)
+
+// console.log(document.querySelector('#heading').innerText)
+
+// console.log($('#heading').innerText) 
